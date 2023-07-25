7f1741063c1af9877ffd7aa891d951e4c83643e8
tatulund <tatu@vaadin.com>
2023-07-25 09:47:34 +0300

    Fine tuning. Style top wndow header. Remember size and pos.

--- src/main/java/com/example/application/components/window/Window.java
+++ src/main/java/com/example/application/components/window/Window.java
@@ -1,11 +1,17 @@
 package com.example.application.components.window;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.dom.DomListenerRegistration;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+
+import elemental.json.JsonObject;
+import elemental.json.JsonValue;
 
 public class Window extends Dialog {
 	boolean mini;
@@ -36,7 +42,7 @@ public class Window extends Dialog {
 			close();
 		});
 		minimizeButton.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_TERTIARY);
-		minimizeButton.addClickListener(e -> {
+		DomListenerRegistration reg = minimizeButton.getElement().addEventListener("click", e -> {
 			mini = !mini;
 			if (mini) {
 				minimize();
@@ -44,11 +50,52 @@ public class Window extends Dialog {
 				restore();
 			}
 		});
+		reg.addEventData("event.stopPropagation()");
 		H3 titleSpan = new H3();
 		titleSpan.setText(title);
 		titleSpan.addClassNames(LumoUtility.TextColor.PRIMARY, LumoUtility.FontSize.MEDIUM,
 				LumoUtility.FontWeight.MEDIUM);
 		header.add(minimizeButton, titleSpan, closeButton);
+
+	}
+
+	@Override
+	public void onAttach(AttachEvent attach) {
+		super.onAttach(attach);
+		getElement().executeJs("""
+				const overlay = this._overlayElement;
+				overlay.addEventListener('mouseup', (e) => {
+				    if (!overlay.getAttribute('class').includes('mini')) {
+				        const dialogs = document.getElementsByTagName('vaadin-dialog-overlay');
+				        var max = 0;
+			          	var maxInd = 0;
+				        for (let i=0;i<dialogs.length;i++) {
+				        if (dialogs[i].classList.contains('window')) {
+				    	    dialogs[i].classList.remove('top');
+				            const ind = Number(dialogs[i].style.zIndex);
+				            if (ind > maxInd) {
+				                maxInd = ind;
+				                max = i;
+				            }  
+				         }
+				    }
+				    dialogs[max].classList.add('top');
+				        const rect = overlay.$.overlay.getBoundingClientRect();
+				        $0.$server.updatePosition(rect);
+				    }
+				    });
+				""", getElement());
+	}
+
+	@ClientCallable
+	public void updatePosition(JsonValue json) {
+		if (!mini) {
+			JsonObject rect = (JsonObject) json;
+			left = rect.getNumber("left") + "px";
+			top = rect.getNumber("top") + "px";
+			width = rect.getNumber("width") + "px";
+			height = rect.getNumber("height") + "px";
+		}
 	}
 
 	public void minimize() {
@@ -76,26 +123,26 @@ public class Window extends Dialog {
 	public void setPosition(String left, String top) {
 		this.left = left;
 		this.top = top;
-		doSetPosition(left, top);
+		if (isOpened()) {
+			doSetPosition(left, top);
+		}
 	}
 
 	private void doSetPosition(String left, String top) {
-		if (isOpened()) {
-			getElement().executeJs("""
-					this._overlayElement.$.overlay.style.left=$0;
-					this._overlayElement.$.overlay.style.top=$1;
-					        """, left, top);
-		}
+		getElement().executeJs("""
+				this._overlayElement.$.overlay.style.left=$0;
+				this._overlayElement.$.overlay.style.top=$1;
+				        """, left, top);
 	}
-	
+
 	@Override
 	public void open() {
 		super.open();
-        setPosition(left, top);
-        setWidth(width);
-        setHeight(height);
-        if (mini) {
-        	restore();
-        }
+		doSetPosition(left, top);
+		setWidth(width);
+		setHeight(height);
+		if (mini) {
+			restore();
+		}
 	}
 }
